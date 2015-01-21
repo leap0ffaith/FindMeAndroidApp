067297bca4db0fc0a0f416565815b84a75405219
Your Name <asif@example.com>
2015-01-21 17:25:08 +0600

    first commit

---
+++ src/com/asif/test/sendsmsfromapptest/MainActivity.java
@@ -0,0 +1,185 @@
+package com.asif.test.sendsmsfromapptest;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+	private MyLocationListener networkLocationListener;
+    private MyLocationListener gpsLocationListener;
+    LocationManager mLocationManager ;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		
+		if(!mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+		{
+			Intent gpsOptionsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);  
+			startActivity(gpsOptionsIntent);
+		}
+		else
+		{
+			Toast.makeText(getApplicationContext(), "network provider enabled", Toast.LENGTH_LONG).show();
+		}
+		
+//		if(!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+//		{
+//			Intent gpsOptionsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);  
+//			startActivity(gpsOptionsIntent);
+//		}
+//		else
+//		{
+//			Toast.makeText(getApplicationContext(), "gps provider enabled", Toast.LENGTH_LONG).show();
+//		}
+		
+//		startLocationListeners();
+	}
+	
+	/*private void startLocationListeners(){
+        // Here we check for "network", "gps" in providers and start them if they are available
+        // Note that "network" is not available in the emulator
+        startLocationListener(networkLocationListener, LocationManager.NETWORK_PROVIDER);
+        startLocationListener(gpsLocationListener, LocationManager.GPS_PROVIDER);
+        mLocationManager.addGpsStatusListener(gpsStatusListener);
+
+        mRunning = true;
+
+    }*/
+	
+	// gps and location code
+	
+	// onClick of the Button
+	public void sendDirectionViaSMS(View v)
+	{
+		Toast.makeText(getApplicationContext(), "onClick", Toast.LENGTH_LONG).show();
+		sendSMS("01847003239","hi asif");
+	}
+	
+	 //---sends an SMS message to another device---
+    private void sendSMS(String phoneNumber, String message)
+    {        
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+ 
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(SENT), 0);
+ 
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(DELIVERED), 0);
+ 
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+ 
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered", 
+                                Toast.LENGTH_SHORT).show();
+                        break;                        
+                }
+            }
+        }, new IntentFilter(DELIVERED));        
+ 
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);        
+    }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	// Methods in this class are called when the location providers give an update
+    private class MyLocationListener implements LocationListener {
+        
+    	@Override
+        public void onLocationChanged(Location location) {
+        	/*if(isBetterLocation(location, currentBestLocation)){
+                currentBestLocation = location;
+                broadcastLocation(location);
+            }*/
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+}
